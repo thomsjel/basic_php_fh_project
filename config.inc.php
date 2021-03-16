7fba650fbcb1367cbc44c321ca4d914801511f3d
thomsjel <63677101+thomsjel@users.noreply.github.com>
2021-03-16 21:20:01 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -2,9 +2,9 @@
 
 require_once ('functions.inc.php');
 
-define("DB_HOST", "mysql5");
-define("DB_NAME", "db_flock-1577_1");
-define("DB_USER", "flock-1577");
-define("DB_PW", "JJGGLTDR8tf7");
+define("DB_HOST", "private");
+define("DB_NAME", "private");
+define("DB_USER", "private");
+define("DB_PW", "private");
 
 define("UPLOADDIR", "./games/");
